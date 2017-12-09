7dcde4552cfd41af2fce82059cc34239f785fb7e
Luckysena <mykolagmd@gmail.com>
2017-12-09 01:22:26 -0500

    autoadd

--- Proj3/server.c
+++ Proj3/server.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv)
       int tempfd = accept(sock_fd,NULL,NULL);
       memcpy(client_fd, &tempfd,sizeof(int));
 
-      pthread_create(&tid,0,acceptService,(void*)&client_fd);
+      pthread_create(&tid,0,acceptService,(void*)client_fd);
     }
     return 0;
 }
