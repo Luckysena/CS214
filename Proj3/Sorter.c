d8386e188f6aeabc2875deeae2e3433babc1e8bf
Luckysena <mykolagmd@gmail.com>
2017-12-09 01:32:15 -0500

    autoadd

--- Proj3/Sorter.c
+++ Proj3/Sorter.c
@@ -395,7 +395,9 @@ void acceptService(int* client_fd){
 	char request[100];
 	char * ack = "Ack";
 	int len = read(client_fd, request, sizeof(request) - 1);
-	printf("Length of request message: %i\n",len);
+	if(len == -1){
+		strerror(errno);
+	}
 	printf("Request received from client id: %i, request: %s\n",*client_fd,request);
 	request[len] = '\0';
 
