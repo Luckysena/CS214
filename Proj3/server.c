5534332cb136fb4619efdd335760313164ec3db7
Luckysena <mykolagmd@gmail.com>
2017-12-08 20:49:46 -0500

    r

--- Proj3/server.c
+++ Proj3/server.c
@@ -1,5 +1,7 @@
 
 #include "Sorter.c"
+pthread_t tid;
+Heap * outputHeap;
 
 int main(int argc, char **argv)
 {
@@ -7,7 +9,12 @@ int main(int argc, char **argv)
       printf("Missing port input, terminating...\n");
       return -1;
     }
+    else if(argc > 2){
+      printf("Too many command arguments, terminating...\n");
+      return -2;
+    }
 
+    char *portNum = argv[1];
     int s;
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -17,7 +24,7 @@ int main(int argc, char **argv)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    s = getaddrinfo(NULL, "1234", &hints, &result);
+    s = getaddrinfo(NULL, portNum, &hints, &result);
     if (s != 0) {
             fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
             exit(1);
@@ -27,26 +34,33 @@ int main(int argc, char **argv)
         perror("bind()");
         exit(1);
     }
+    while(true){
+      if (listen(sock_fd, 10) != 0) {
+          perror("listen()");
+          exit(1);
+      }
 
-    if (listen(sock_fd, 10) != 0) {
-        perror("listen()");
-        exit(1);
+      struct sockaddr_in *result_addr = (struct sockaddr_in *) result->ai_addr;
+      printf("Listening on file descriptor %d, port %d\n", sock_fd, ntohs(result_addr->sin_port));
+
+      printf("Waiting for connection...\n");
+      int * client_fd = (int*)malloc(sizeof(int));
+      memcpy(client_fd, accept(sock_fd,NULL,NULL),sizeof(int));
+
+      printf("%i\n",*client_fd);
+      //pthread_create(&tid,0,acceptService,(void*)&client_fd);
+
+      printf("Connection made: client_fd=%d\n", client_fd);
+
+
+      char buffer[1000];
+      int len = read(client_fd, buffer, sizeof(buffer) - 1);
+      buffer[len] = '\0';
+
+
+
     }
 
-    struct sockaddr_in *result_addr = (struct sockaddr_in *) result->ai_addr;
-    printf("Listening on file descriptor %d, port %d\n", sock_fd, ntohs(result_addr->sin_port));
-
-    printf("Waiting for connection...\n");
-    int client_fd = accept(sock_fd, NULL, NULL);
-    printf("Connection made: client_fd=%d\n", client_fd);
-
-    char buffer[1000];
-    int len = read(client_fd, buffer, sizeof(buffer) - 1);
-    buffer[len] = '\0';
-
-    printf("Read %d chars\n", len);
-    printf("===\n");
-    printf("%s\n", buffer);
 
     return 0;
 }
